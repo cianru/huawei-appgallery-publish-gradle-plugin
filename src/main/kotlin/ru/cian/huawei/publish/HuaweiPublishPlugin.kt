a172f1391101fa8179b4c2b8a759276f52666dce
Cedrick Flocon <7161169+CedrickFlocon@users.noreply.github.com>
2020-11-09 10:22:32 +0100

    Create task after evaluate project. To be sure Android Gradle Plugin was applied.

--- src/main/kotlin/ru/cian/huawei/publish/HuaweiPublishPlugin.kt
+++ src/main/kotlin/ru/cian/huawei/publish/HuaweiPublishPlugin.kt
@@ -9,11 +9,6 @@ import org.gradle.api.Project
 class HuaweiPublishPlugin : Plugin<Project> {
 
     override fun apply(project: Project) {
-
-        check(project.plugins.hasPlugin("com.android.application")) {
-            "Plugin must be applied to the main app plugin but was applied to ${project.path}"
-        }
-
         project.extensions.create(
             HuaweiPublishExtension.MAIN_EXTENSION_NAME,
             HuaweiPublishExtension::class.java,
@@ -22,17 +17,16 @@ class HuaweiPublishPlugin : Plugin<Project> {
 
         project.afterEvaluate {
             if (!project.plugins.hasPlugin(AppPlugin::class.java)) {
-                project.logger.warn(
-                    "The Android Gradle Plugin was not applied. Gradle Play Publisher " +
-                        "will not be configured.")
+                throw IllegalStateException(
+                    "The Android Gradle Plugin was not applied. Huawei publish will not be configured."
+                )
             }
-        }
-
-        val androidExtension = project.extensions.getByType(AppExtension::class.java)
-        androidExtension.applicationVariants.all { variant ->
-            if (!variant.buildType.isDebuggable) {
-                createTask(project, variant)
-            }
+            project.extensions.getByType(AppExtension::class.java)
+                .applicationVariants.all { variant ->
+                    if (!variant.buildType.isDebuggable) {
+                        createTask(project, variant)
+                    }
+                }
         }
     }
 
